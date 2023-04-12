e7328c4ce1faadc5dfb72ba6294849d21bf788f8
User <User@ithub.tulskaya>
2023-04-12 18:39:17 +0300

    batootes

---
+++ PAPinball/Assets/scripts/Batutes.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Batutes : MonoBehaviour
+{
+    [SerializeField] private float _strength;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            Debug.Log("asdasd");
+            Vector3 ballOldDirection = transform.position - collision.gameObject.transform.position;
+            float dist = ballOldDirection.magnitude;
+            Vector3 ballNewDirection = ballOldDirection / dist;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(ballNewDirection.x * _strength, ballNewDirection.y * _strength, ballNewDirection.z * _strength), ForceMode.Impulse);
+        }
+    }
+}   
